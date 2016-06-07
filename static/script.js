91b5044eee8403933d4746dc9de0500a8e0c70e8
Jesús Carpintero Rodríguez <j.carpintero@alumnos.upm.es>
2016-06-07 10:22:18 +0200

    Window Size

--- static/script.js
+++ static/script.js
@@ -1,9 +1,22 @@
 
-    var win    = $( this );
-    var width  = api.tool.desktopWidth();
-    var height = api.tool.desktopHeight() - 70;
-    var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
-    var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
+  var win    = $( this );
+  var width;
+  var height;
+  var minWidth  = 1080;
+  var minHeight = 629;
+  var maxWidth = api.tool.desktopWidth();
+  var maxHeigth = api.tool.desktopHeight() - 70;
+
+  if (minHeight > maxHeigth || minWidth > maxWidth) {
+    width = maxWidth;
+    height = maxHeigth;
+
+  }else {
+    width = minWidth;
+    height = minHeight;
+  }
+  var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
+  var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
     var windowObject = api.popup( 'https://facebook.com/', width, height ).render();
 
     var timer = setInterval( function(){
